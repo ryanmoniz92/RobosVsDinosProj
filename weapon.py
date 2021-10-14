4c9585fd1512e39ec21952c3b7bd324f9b7829c3
Ryan Moniz <ryanmoniz@Ryans-MacBoook-Air.local>
2021-10-14 15:42:12 -0400

    modified weapons class to randomly select weapon from list

--- weapon.py
+++ weapon.py
@@ -1,5 +1,8 @@
+import random
+
+robot_weapons = ['Electric Shock', 'Plasma Projectile', 'Circuit Board Crusher']
+
 class Weapon:
     
-    def __init__(self, name, attack_power):
-        self.name = ''
-        self.attack_power = int
+    def __init__(self):
+        self.name = random.choice(list(robot_weapons))
